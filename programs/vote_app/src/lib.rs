faeaa114c20246271fb3f0f68898f44b109a5f55
Samrat <ksamrat224@gmail.com>
2026-02-07 22:26:10 +0545

    refactor: initialize treasury with sol_price and tokens_per_purchase

--- programs/vote_app/src/lib.rs
+++ programs/vote_app/src/lib.rs
@@ -7,10 +7,18 @@ declare_id!("3jfJ7Kd6hryKHjoCQeTWsrVuoDgoAz5JJ18MbxL45V2c");
 
 #[program]
 pub mod vote_app {
+    use crate::state::TreasuryConfig;
+
     use super::*;
-    pub fn initialize_treasury(ctx: Context<InitializeTreasury>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    pub fn initialize_treasury(ctx: Context<InitializeTreasury>, sol_price: u64,tokens_per_purchase: u64) -> Result<()> {
+       let treasury_config_account: &mut Account<'_, TreasuryConfig> = &mut  ctx.accounts.treasury_config_account;
+       treasury_config_account.authority = ctx.accounts.authority.key();
+       treasury_config_account.bump = ctx.bumps.sol_vault;
+       treasury_config_account.sol_price = sol_price;
+       treasury_config_account.x_mint = ctx.accounts.x_mint.key();
+       treasury_config_account.tokens_per_purchase = tokens_per_purchase;
+       
+       Ok(())
     }
 }
 
