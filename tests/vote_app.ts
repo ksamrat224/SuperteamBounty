1bb4986099b1648f7618ab48029711cab1a451b6
Samrat <ksamrat224@gmail.com>
2026-02-09 22:02:00 +0545

    test: add vote_app integration tests and update dependencies

--- tests/vote_app.ts
+++ tests/vote_app.ts
@@ -1,16 +1,24 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { VoteApp } from "../target/types/vote_app";
+import { expect }  from "chai";
 
+import {getOrCreateAssociatedTokenAccount,getAccount} from "@solana/spl-token";
+import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
 describe("vote_app", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.voteApp as Program<VoteApp>;
 
-  it("Is initialized!", async () => {
+  it("initializes treasury!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initializeTreasury()
+      .accounts({
+        treasuryConfigAccount,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
   });
 });
