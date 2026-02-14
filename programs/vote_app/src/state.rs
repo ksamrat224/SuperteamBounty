8f08549337d45b9f95e8f8c103019e341370abbf
Samrat <ksamrat224@gmail.com>
2026-02-14 19:25:37 +0545

    refactor: add error codes, events, and context updates to vote_app

--- programs/vote_app/src/state.rs
+++ programs/vote_app/src/state.rs
@@ -1,30 +1,46 @@
 use anchor_lang::prelude::*;
-
 #[account]
 #[derive(InitSpace)]
-pub struct TreasuryConfig {
+pub struct TreasuryConfig{
     pub authority: Pubkey,
-    pub x_mint: Pubkey, //tokens it will deal
-    pub treasury_token_account: Pubkey, //will hold the tokens
-    pub sol_price :u64,
-    pub tokens_per_purchase: u64,
-    pub bump: u8,
+    pub x_mint: Pubkey,
+    pub treasury_token_account: Pubkey, 
+    pub sol_price: u64, 
+    pub tokens_per_purchase: u64, 
+    pub bump: u8, 
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Voter  {
-    pub voter_id:Pubkey,
+pub struct Voter {
+    pub voter_id: Pubkey,
     pub proposal_voted: u8,
 }
 
 #[account]
 #[derive(InitSpace)]
-pub struct Proposal {
+pub struct Proposal{
     pub proposal_id: u8,
     pub number_of_votes: u8,
     pub deadline: i64,
-    #[max_len(200)]
+    #[max_len(50)]
     pub proposal_info: String,
     pub authority: Pubkey,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct ProposalCounter {
+    pub authority: Pubkey,
+    pub proposal_count: u8,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Winner {
+    pub winning_proposal_id: u8,
+    pub winning_votes: u8,
+    #[max_len(50)]
+    pub proposal_info: String,
+    pub declared_at: i64,
 }
